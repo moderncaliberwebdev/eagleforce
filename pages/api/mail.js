6caabc8a914ef72a7af222ef2996b4f9676e1b11
Caleb Martin <cmartin@moderncaliber.com>
2022-10-26 08:43:43 -0400

    contact page

---
+++ pages/api/mail.js
@@ -0,0 +1,56 @@
+import nodemailer from 'nodemailer'
+import mailGun from 'nodemailer-mailgun-transport'
+import validator from 'validator'
+
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_API_DOMAIN,
+  },
+}
+
+const transporter = nodemailer.createTransport(mailGun(auth))
+
+const mailTo = (firstname, lastname, email, phone, message, callback) => {
+  //validation
+  if (!firstname || !lastname || !email || !phone || !message) {
+    return callback('Please fill in all fields', undefined)
+  } else if (!validator.isEmail(email)) {
+    return callback('Provide a valid email', undefined)
+  } else {
+    const output = `
+                <h3>First Name: </h3> ${firstname}
+                <h3>Last Name: </h3> ${lastname}
+                <h3>Email: </h3> ${email}
+                <h3>Phone Number: </h3> ${phone}
+                <h3>Message: </h3> ${message}
+            `
+    const mailOptions = {
+      from: email,
+      to: 'cmartin@moderncaliber.com',
+      subject: 'Eagle Force Contact Request',
+      html: output,
+    }
+    transporter.sendMail(mailOptions, (err, data) => {
+      if (err) {
+        callback('Internal Error', undefined)
+      } else {
+        callback(undefined, data)
+      }
+    })
+  }
+}
+
+export default function mail(req, res) {
+  const { firstname, lastname, email, phone, message } = req.query
+  mailTo(firstname, lastname, email, phone, message, (err, data) => {
+    res.send({
+      firstname,
+      lastname,
+      email,
+      phone,
+      message,
+      formResponse: err,
+    })
+  })
+}
---
+++ pages/contact.js
@@ -0,0 +1,114 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import Layout from '../Components/Layout'
+import styles from '../styles/Contact.module.scss'
+
+import { getAuth } from 'firebase/auth'
+import app from '../firebase/clientApp'
+import axios from 'axios'
+
+export default function Home({}) {
+  const auth = getAuth(app)
+  const user = auth.currentUser
+
+  const formSubmit = async () => {
+    const firstname = document.getElementById('form-firstname')
+    const lastname = document.getElementById('form-lastname')
+    const email = document.getElementById('form-email')
+    const phone = document.getElementById('form-phone')
+    const message = document.getElementById('form-message')
+    const formResponse = document.getElementById('form-response')
+    const formSuccess = document.getElementById('form-success')
+
+    const inputs = [firstname, lastname, email, phone, message]
+
+    const { data } = await axios.get(
+      `/api/mail?firstname=${firstname.value}&lastname=${lastname.value}&email=${email.value}&phone=${phone.value}&message=${message.value}`
+    )
+
+    if (data.formResponse) {
+      formResponse.textContent = data.formResponse
+    } else {
+      formSuccess.textContent = 'Message Sent'
+      formResponse.textContent = ''
+      inputs.forEach((input) => {
+        input.value = ''
+      })
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Contact | Eagle Force Employment Services</title>
+        <meta
+          name='description'
+          content='Any questions or remarks? Just send us a message!'
+        />
+        <meta
+          property='og:title'
+          content='Contact | Eagle Force Employment Services'
+        />
+        <meta
+          property='og:description'
+          content='Any questions or remarks? Just send us a message!'
+        />
+        <meta
+          property='og:url'
+          content='https://www.eagleforceemploymentservices.com/'
+        />
+        <meta property='og:type' content='website' />
+        <link rel='icon' href='/images/layout/logo.png' />
+      </Head>
+      <Layout>
+        <div className={styles.contact}>
+          <h1 className={styles.contact__h1}>Contact</h1>
+          <p className={styles.contact__p}>
+            Any questions or remarks? Just write us a message!
+          </p>
+          <form className={styles.contact__form}>
+            <div className={styles.contact__form__input_short}>
+              <p>First Name</p>
+              <input type='text' id='form-firstname' />
+            </div>
+            <div className={styles.contact__form__input_short}>
+              <p>Last Name</p>
+              <input type='text' id='form-lastname' />
+            </div>
+            <div className={styles.contact__form__input_short}>
+              <p>Email</p>
+              <input type='text' id='form-email' />
+            </div>
+            <div className={styles.contact__form__input_short}>
+              <p className={styles.contact__form__input_short__p}>
+                Phone Number
+              </p>
+              <input type='text' id='form-phone' />
+            </div>
+            <div className={styles.contact__form__input_long}>
+              <p>Message</p>
+              <input type='text' id='form-message' />
+            </div>
+            <div
+              id='form-response'
+              className={styles.contact__form__response}
+            ></div>
+            <div
+              id='form-success'
+              className={styles.contact__form__success}
+            ></div>
+          </form>
+          <div className={styles.contact__buttons}>
+            <button
+              className={styles.orange_button}
+              onClick={() => formSubmit()}
+            >
+              Send Message
+            </button>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  )
+}
